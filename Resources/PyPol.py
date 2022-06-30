50fe10fbb33d04cd02a2c22c873227d700975ff7
halesoysters <107145446+halesoysters@users.noreply.github.com>
2022-06-30 15:26:03 -0600

    Update PyPol.py

--- Resources/PyPol.py
+++ Resources/PyPol.py
@@ -5,15 +5,23 @@ file_to_load = os.path.join("bootcamp/class folder/week 3/Election_Analysis/Reso
 # Create a filename variable to a direct opath to the file.
 file_to_save = os.path.join("bootcamp/class folder/week 3/Election_Analysis/Resources/Analysis/election_analysis.txt")
 
+# 1. Initialize total vote counter
+total_votes = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-
-     #read and analyze the data here:
     file_reader = csv.reader(election_data)
 
     # Read and Print the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file
+    for row in file_reader:
+            #2. Add to the total vote count
+            total_votes += 1
+
+    # 3. 
+    print(total_votes)
 
 
 
